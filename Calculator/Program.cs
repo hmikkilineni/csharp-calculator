d938b0f373f64ea766f76dee668cfe46fa780e04
Harisri-Mikkilineni <harisri.mikkilineni@gmail.com>
2026-03-26 10:37:21 +0100

    Add calculator project

---
+++ Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Enter your first number: ");
+double number1 = double.Parse(Console.ReadLine());
+
+Console.Write("Enter your arithmetic operator (+, -, *, /): ");
+string operatorInput = Console.ReadLine();
+
+Console.Write("Enter your second number: ");
+double number2 = double.Parse(Console.ReadLine());
+
+if(operatorInput == "+")
+{
+    Console.WriteLine($"Result: {number1} + {number2} = {number1 + number2}");
+}else if(operatorInput == "-")
+{
+    Console.WriteLine($"Result: {number1} - {number2} = {number1 - number2}");
+}
+else if (operatorInput == "*")
+{
+    Console.WriteLine($"Result: {number1} * {number2} = {number1 * number2}");
+}
+else if (operatorInput == "/")
+{
+    if(number2 == 0)
+    {
+        Console.WriteLine("Error: Cannot Divide by zero!");
+    }
+    else
+    {
+        Console.WriteLine($"Result: {number1} / {number2} = {number1 / number2}");
+    }
+
+}
+else
+{
+    Console.WriteLine("Invalid operator! Use +, -, *, /");
+}
