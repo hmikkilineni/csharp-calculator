7d4d2e76d434e8f2e66f39620ce18425fce8ec2d
Harisri-Mikkilineni <harisri.mikkilineni@gmail.com>
2026-03-31 22:39:19 +0200

    Add error handling to calculator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,38 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+double number1 = 0;
+double number2 = 0;
+string operatorInput = "";
 
-Console.Write("Enter your first number: ");
-double number1 = double.Parse(Console.ReadLine());
-
-Console.Write("Enter your arithmetic operator (+, -, *, /): ");
-string operatorInput = Console.ReadLine();
-
-Console.Write("Enter your second number: ");
-double number2 = double.Parse(Console.ReadLine());
-
-if(operatorInput == "+")
+try
 {
+    Console.Write("Enter your first number: ");
+   number1 = double.Parse(Console.ReadLine());
+
+    Console.Write("Enter your arithmetic operator (+, -, *, /): ");
+    operatorInput = Console.ReadLine();
+
+    Console.Write("Enter your second number: ");
+     number2 = double.Parse(Console.ReadLine());
+
+   if (operatorInput == "+")
+   {
     Console.WriteLine($"Result: {number1} + {number2} = {number1 + number2}");
-}else if(operatorInput == "-")
-{
+   }
+   else if (operatorInput == "-")
+   {
     Console.WriteLine($"Result: {number1} - {number2} = {number1 - number2}");
-}
-else if (operatorInput == "*")
-{
+   }
+   else if (operatorInput == "*")
+   {
     Console.WriteLine($"Result: {number1} * {number2} = {number1 * number2}");
-}
-else if (operatorInput == "/")
-{
-    if(number2 == 0)
-    {
-        Console.WriteLine("Error: Cannot Divide by zero!");
-    }
-    else
-    {
-        Console.WriteLine($"Result: {number1} / {number2} = {number1 / number2}");
-    }
+   }
 
+   else if (operatorInput == "/")
+   {
+        if (number2 == 0)
+            throw new DivideByZeroException();
+        Console.WriteLine($"Result: {number1} / {number2} = {number1 / number2}");
+   }
+    else
+        Console.WriteLine("Invalid operator!");
 }
-else
+catch (FormatException ex)
 {
-    Console.WriteLine("Invalid operator! Use +, -, *, /");
+    Console.WriteLine("Please enter a valid number!");
 }
+ catch (DivideByZeroException ex){
+        Console.WriteLine("Cannot divide by zero!");
+    }
+finally
+{
+    Console.WriteLine("Thank you for using the calculator!");
+}
+Console.ReadKey();
